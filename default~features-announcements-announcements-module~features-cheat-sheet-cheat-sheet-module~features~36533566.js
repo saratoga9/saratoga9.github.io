7636b8dee526ddd3f0cdad78631ae7b20161d972
hadjigamil <hadjigamil@gmail.com>
2021-09-12 11:03:26 -0600

    auto deployment updates

--- default~features-announcements-announcements-module~features-cheat-sheet-cheat-sheet-module~features~36533566.js
+++ default~features-announcements-announcements-module~features-cheat-sheet-cheat-sheet-module~features~36533566.js
@@ -738,7 +738,7 @@ DATA.cheatSheet = {
         endTime: '1:00 PM',
         address: DATA.addresses.chapel,
         presiding: DATA.people.bp,
-        conducting: DATA.people.bp2ndc,
+        conducting: DATA.people.bp1stc,
         chorister: DATA.people.chorister1,
         organist: DATA.people.organist1,
         invocation: DATA.people.tbd,
